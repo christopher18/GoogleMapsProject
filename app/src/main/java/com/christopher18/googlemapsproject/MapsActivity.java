68115edc90559986f6d19a4d07dc8245ee6c5ae3
クリス <christopheracker1@gmail.com>
2016-08-10 22:17:13 -0500

    custom marker addition functionality added

--- app/src/main/java/com/christopher18/googlemapsproject/MapsActivity.java
+++ app/src/main/java/com/christopher18/googlemapsproject/MapsActivity.java
@@ -2,6 +2,8 @@ package com.christopher18.googlemapsproject;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +15,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +50,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    /*
+     * Excecutes on button click to add a location.
+     *  Adds marker at location specified with the name specified.
+     */
+    public void addTag (View view) {
+        EditText lat = (EditText)findViewById(R.id.latitude);
+        latitude = Integer.parseInt(lat.getText().toString());
+
+        EditText lon = (EditText)findViewById(R.id.longitude);
+        longitude = Integer.parseInt(lon.getText().toString());
+
+        EditText nam = (EditText)findViewById(R.id.name);
+        name = nam.getText().toString();
+
+        LatLng place = new LatLng(latitude, longitude);
+
+        mMap.addMarker(new MarkerOptions().position(place).title(name));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
     }
 }
