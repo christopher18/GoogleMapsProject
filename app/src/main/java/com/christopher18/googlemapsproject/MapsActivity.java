b446b922c2d0ed2abcc62d84c346a7ebf80a897d
クリス <christopheracker1@gmail.com>
2016-08-28 15:58:49 -0500

    marker locations can now be randomly generated and set within a defined range

--- app/src/main/java/com/christopher18/googlemapsproject/MapsActivity.java
+++ app/src/main/java/com/christopher18/googlemapsproject/MapsActivity.java
@@ -1,26 +1,49 @@
 package com.christopher18.googlemapsproject;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Random;
+
+public class MapsActivity extends AppCompatActivity
+        implements OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private boolean mPermissionDenied = false;
 
     private GoogleMap mMap;
 
-    private double latitude;
+    private int min = 0;
 
-    private double longitude;
-
-    private String name;
+    private int max = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,28 +70,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+        // adds 10 tags in random locations within a defined range (defined by min and max int values)
+        addTags();
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // camera animates to user's current position
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission not granted, display error dialog
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog.newInstance(true).show(getSupportFragmentManager(),
+                "Please activate GPS to see your location.");
     }
     /*
      * Excecutes on button click to add a location.
      *  Adds marker at location specified with the name specified.
      */
-    public void addTag (View view) {
-        EditText lat = (EditText)findViewById(R.id.latitude);
-        latitude = Integer.parseInt(lat.getText().toString());
+    public void addTags() {
+        Random randNum = new Random();
 
-        EditText lon = (EditText)findViewById(R.id.longitude);
-        longitude = Integer.parseInt(lon.getText().toString());
 
-        EditText nam = (EditText)findViewById(R.id.name);
-        name = nam.getText().toString();
-
-        LatLng place = new LatLng(latitude, longitude);
-
-        mMap.addMarker(new MarkerOptions().position(place).title(name));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
+        for (int i = 0; i < 10; i++) {
+            LatLng place = new LatLng(randNum.nextInt((max - min) + 1) + min,
+                    randNum.nextInt((max - min) + 1) + min);
+            mMap.addMarker(new MarkerOptions().position(place).title("marker " + i));
+        }
     }
 }
